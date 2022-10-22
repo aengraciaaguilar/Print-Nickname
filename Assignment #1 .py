e9dc51adf874e4f376e9e4c9bc2d34adeedcff25
Aengracia Amil Ica Y. Aguilar <116430572+aengraciaaguilar@users.noreply.github.com>
2022-10-22 18:47:09 +0800

    Add files via upload

---
+++ Assignment #1 .py
@@ -0,0 +1,10 @@
+# Assignment #1  
+# Name: Aguilar, Aengracia
+# Year and Section: BSCOE 2-2 
+
+print(" ********  *******   *******")
+print(" ********  *******   **   **")
+print("    **     **        *******")
+print("    **     **        *******")
+print(" ********  *******   **   **")
+print(" ********  *******   **   **")
